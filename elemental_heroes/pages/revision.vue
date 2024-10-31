348ce9ad42627462b0c35e990e0c28189d4cc3af
Asher <110836734+influasher@users.noreply.github.com>
2024-11-01 01:32:06 +0800

    minor fixes

--- elemental_heroes/pages/revision.vue
+++ elemental_heroes/pages/revision.vue
@@ -4,7 +4,7 @@
     <!-- Top Section -->
     <div class="mb-5">
       <div class="d-flex justify-content-between align-items-center mb-4">
-        <h2 class="section-title">Announcement</h2>
+        <h2 class="section-title">My Notes</h2>
         <NuxtLink to="/upload">
           <button class="btn btn-primary">Upload Notes</button>
         </NuxtLink>
