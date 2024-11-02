b04d6dbedee71ff923f4681c6887d9956f57a4d4
Asher <110836734+influasher@users.noreply.github.com>
2024-11-02 23:10:00 +0800

    notes and revision.vue enhancementS

--- elemental_heroes/pages/notes.vue
+++ elemental_heroes/pages/notes.vue
@@ -1,30 +1,51 @@
 <template>
-    <div class="notes-page">
-        <div class="d-flex justify-content-between align-items-center mb-4">
-        <h1>{{ noteName }}</h1>
-        <NuxtLink :to="{ path: '/flashcard', query: { name: noteName } }">
-          <button class="btn btn-primary">Generate Flashcards</button>
-        </NuxtLink>
-      </div>
+  <div class="notes-page">
+    <div class="d-flex justify-content-between align-items-center mb-4">
+      <h1>{{ noteName }}</h1>
+      <NuxtLink :to="{ path: '/flashcard', query: { name: noteName } }">
+        <button class="btn btn-primary">Generate Flashcards</button>
+      </NuxtLink>
+    </div>
     <div class="d-flex justify-content-center mb-4">
-        <iframe 
-            class="d2l-fileviewer-rendered-pdf" 
-            scrolling="no" 
-            seamless="seamless" 
-            src="/assets/Session9.Stock Valuation.pdf"
-            style="width: 80%; height: 651px;">
-        </iframe>
+      <iframe
+        class="d2l-fileviewer-rendered-pdf"
+        scrolling="no"
+        seamless="seamless"
+        :src="noteUrl"
+        style="width: 80%; height: 651px"
+      >
+      </iframe>
     </div>
-    </div>
-
-
+  </div>
 </template>
 
 <script setup>
-import { useRoute } from 'vue-router';
+import { useRoute } from "vue-router";
+import { createClient } from "@supabase/supabase-js";
+import { useRuntimeConfig } from "#app";
 
+const config = useRuntimeConfig();
+const supabase = createClient(
+  config.public.supabaseUrl,
+  config.public.supabaseKey
+);
 const route = useRoute();
 const noteName = route.query.name; // Retrieve the `name` query parameter
+const noteUrl = ref(null);
+
+async function getPDF() {
+  const { data, error } = await supabase.storage
+    .from("files_wad2")
+    .getPublicUrl(`user_pdfs/` + noteName + ".pdf");
+  if (data) {
+    noteUrl.value = data.publicUrl;
+    console.log(noteUrl.value);
+  }
+}
+
+onMounted(() => {
+  getPDF();
+});
 </script>
 
 <style scoped>
